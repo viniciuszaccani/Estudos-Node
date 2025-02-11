7c19ecf1ddfd30f22a29226063c868259d28a794
Vinicius Zaccani <vinicius.zaccani@gmail.com>
2025-02-11 16:34:49 -0300

    continuando estudos

---
+++ 3_Core_Modules/2_retornando_html/index.js
@@ -0,0 +1,13 @@
+const http = require("http")
+
+const port = 3000
+
+const server = http.createServer( (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('contenty-type', 'text/html')
+    res.end('<h1>Olá, este é o meu primeiro server com HTML!</h1> <p>teste</p>')
+})
+
+server.listen(port, ()=> {
+    console.log(`Servidor rodando na porta: ${port}`)
+})
